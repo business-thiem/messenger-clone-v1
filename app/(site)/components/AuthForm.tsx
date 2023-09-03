208076164e65f27c9afdf205beac7dafd8a11860
Thiem <thiem.huynh0@gmail.com>
2023-09-03 16:01:41 -0400

    form data type setup

--- app/(site)/components/AuthForm.tsx
+++ app/(site)/components/AuthForm.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import Input from "@/app/components/inputs/Input";
 import { useCallback, useState } from "react";
 import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
 
@@ -41,7 +42,19 @@ const AuthForm = () => {
     }
   };
 
-  return <div>Auth Form2</div>;
+  const socialAction = (action: string) => {
+    setIsLoading(true);
+  };
+
+  return (
+    <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
+      <div className="bg-white px-4 py-8 shadow sm:rounded-lg sm:px-10">
+        <form className="space-y-6" onSubmit={handleSubmit(onSubmit)}>
+          <Input label="Email" />
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default AuthForm;
