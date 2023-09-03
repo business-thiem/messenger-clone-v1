619ce2a794c95b2dfc701a70ae3c04c77cb08fec
Thiem <thiem.huynh0@gmail.com>
2023-09-03 15:34:32 -0400

    login page init

---
+++ app/(site)/page.tsx
@@ -0,0 +1,18 @@
+import { GiMagicHat } from "react-icons/gi";
+
+export default function Home() {
+  return (
+    <div className="flex min-h-full flex-col justify-center py-12 sm:px-6 lg:px-8 bg-gray-500">
+      {" "}
+      Index PAge
+      <div className="sm:mx-auto sm:w-full sm:max-w-md">
+        <GiMagicHat alt="Logo" className="mx-auto w-auto text-9xl" />
+      </div>
+      <h2 className="mt-6 text-center text-3xl font-bold tracking-tiht text-gray-900">
+        {" "}
+        Sign into your Account
+      </h2>
+    </div>
+    // auth form
+  );
+}
