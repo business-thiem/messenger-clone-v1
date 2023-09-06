23ce07de42a657fcc9498d88b20423502bb394f7
Thiem <thiem.huynh0@gmail.com>
2023-09-06 18:59:36 -0400

    reenabled form after login attempt

--- app/(site)/components/AuthForm.tsx
+++ app/(site)/components/AuthForm.tsx
@@ -50,20 +50,34 @@ const AuthForm = () => {
       signIn("credentials", {
         ...data,
         redirect: false,
-      }).then((callback) => {
-        if (callback?.error) {
-          toast.error("Invalid credentials");
-        }
+      })
+        .then((callback) => {
+          if (callback?.error) {
+            toast.error("Invalid credentials");
+          }
 
-        if (callback?.ok && !callback?.error) {
-          toast.success("Logged In!");
-        }
-      });
+          if (callback?.ok && !callback?.error) {
+            toast.success("Logged In!");
+          }
+        })
+        .finally(() => setIsLoading(false));
     }
   };
 
   const socialAction = (action: string) => {
     setIsLoading(true);
+
+    signIn(action, { redirect: false })
+      .then((callback) => {
+        if (callback?.error) {
+          toast.error("Invalid Credentials");
+        }
+
+        if (callback?.ok && !callback?.error) {
+          toast.success("Logged in!");
+        }
+      })
+      .finally(() => setIsLoading(false));
   };
 
   return (
