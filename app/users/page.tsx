1b7339cadf518398f3c0ddd84d12f14c2c4dfe65
Thiem <thiem.huynh0@gmail.com>
2023-09-07 17:24:59 -0400

    made signout function call instead of instance cal

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -3,11 +3,7 @@
 import { signOut } from "next-auth/react";
 
 const Users = () => {
-  return (
-    <div>
-      <button onClick={() => signOut}>Logout</button>
-    </div>
-  );
+  return <button onClick={() => signOut()}>Logout</button>;
 };
 
 export default Users;
